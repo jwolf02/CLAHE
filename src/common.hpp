4420ba1007c495c313ec6334dab4678d29f84aa1
jwolf02 <jonaswolf@startmail.com>
2020-09-06 23:16:53 +0200

    finished testbench

---
+++ src/common.hpp
@@ -0,0 +1,330 @@
+#ifndef __UTIL_HPP
+#define __UTIL_HPP
+
+#include <vector>
+#include <string>
+#include <type_traits>
+#include <stdexcept>
+#include <algorithm>
+#include <iostream>
+#include <functional>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <fcntl.h>
+#include <unistd.h>
+#include <random>
+
+namespace string {
+
+    /***
+     * split string by the specified delimiter, if delim is the empty string
+     * then a vector of chars is returned
+     * @param str the string to split
+     * @param delim the delimiter, by default a single space
+     * @return a vector of strings
+     */
+    inline std::vector<std::string> split(const std::string &str, const std::string &delim = " ") {
+        if (delim.empty()) {
+            throw std::runtime_error("empty delimiter");
+        }
+        std::vector<std::string> tokens;
+        size_t last = 0;
+        size_t next = 0;
+        while ((next = str.find(delim, last)) != std::string::npos) {
+            tokens.emplace_back(str.substr(last, next - last));
+            last = next + 1;
+        }
+        tokens.emplace_back(str.substr(last));
+        return tokens;
+    }
+
+    /***
+     * check if str1 starts with str2
+     * @param str1
+     * @param str2
+     * @return
+     */
+    inline bool starts_with(const std::string &str1, const std::string &str2) {
+        return str1.size() >= str2.size() && std::equal(str1.begin(), str1.begin() + str2.size(), str2.begin());
+    }
+
+    /***
+     * check if str1 end with str2
+     * @param str1
+     * @param str2
+     * @return
+     */
+    inline bool ends_with(const std::string &str1, const std::string &str2) {
+        return str1.size() >= str2.size() && std::equal(str1.end() - str2.size(), str1.end(), str2.begin());
+    }
+
+    /***
+     * equals ignore case
+     * @param str1
+     * @param str2
+     * @return
+     */
+    inline bool iequals(const std::string &str1, const std::string &str2) {
+        return str1.size() == str2.size() &&
+               std::equal(str1.begin(), str1.end(), str2.begin(), [](char a, char b) -> bool {
+                   return std::tolower(a) == std::tolower(b);
+               });
+    }
+
+    inline bool contains(const std::string &str1, const std::string &str2) {
+        return str1.size() <= str2.size() && str1.find_first_of(str2, 0) != std::string::npos;
+    }
+
+    /***
+     * cast string to arithmetic type specified by the template argument
+     * @tparam T type to case to
+     * @param str string to be casted
+     * @return arithmetic type
+     */
+    template<typename T>
+    constexpr T to(const std::string &str) {
+        static_assert(std::is_fundamental<T>::value && (std::is_arithmetic<T>::value || std::is_same<T, bool>::value),
+                      "cannot convert std::string to non-arithmetic type");
+
+        // signed types
+        if (std::is_same<T, char>::value || std::is_same<T, short>::value || std::is_same<T, int>::value ||
+            std::is_same<T, long>::value || std::is_same<T, long long>::value) {
+            return (T) std::strtoll(str.c_str(), nullptr, 10);
+        } // unsigned types
+        else if (std::is_same<T, unsigned char>::value || std::is_same<T, unsigned short>::value ||
+                 std::is_same<T, unsigned int>::value ||
+                 std::is_same<T, unsigned long>::value || std::is_same<T, unsigned long long>::value) {
+            return (T) std::strtoull(str.c_str(), nullptr, 10);
+        } // double
+        else if (std::is_same<T, double>::value) {
+            return std::strtod(str.c_str(), nullptr);
+        } // float
+        else if (std::is_same<T, float>::value) {
+            return std::strtof(str.c_str(), nullptr);
+        } // long double
+        else if (std::is_same<T, long double>::value) {
+            return std::strtold(str.c_str(), nullptr);
+        } // bool
+        else if (std::is_same<T, bool>::value) {
+            if (str == "true" || str == "1" || str == "True" || str == "TRUE")
+                return true;
+            else if (str == "false" || str == "0" || str == "False" || str == "FALSE")
+                return false;
+            else
+                throw std::runtime_error("cannot cast std::string '" + str + "' to bool");
+        } else {
+            throw std::domain_error("cannot convert std::string to specified type");
+        }
+    }
+
+    /***
+     * remove leading and trailing whitespace characters (can be any characters) from string
+     * @param str
+     * @param whitespace
+     * @return
+     */
+    inline std::string trim(const std::string &str, const std::string &whitespace = " \t\n\r") {
+        const size_t begin = str.find_first_not_of(whitespace);
+        const size_t end = str.find_last_not_of(whitespace);
+        return str.substr(begin, end - begin + 1);
+    }
+
+    /***
+     * convert string to upper case
+     * @param str
+     * @return
+     */
+    inline std::string to_upper(const std::string &str) {
+        std::string up = str;
+        std::transform(str.begin(), str.end(), up.begin(), ::toupper);
+        return up;
+    }
+
+    /***
+     * convert string to lower case
+     * @param str
+     * @return
+     */
+    inline std::string to_lower(const std::string &str) {
+        std::string lo = str;
+        std::transform(str.begin(), str.end(), lo.begin(), ::tolower);
+        return lo;
+    }
+}
+
+namespace seq {
+
+    template<typename iterator>
+    inline decltype(iterator::operator*) min(iterator begin, iterator end) {
+        if (end - begin == 0) {
+            throw std::length_error("empty sequence");
+        }
+
+        iterator min = begin;
+        for (auto it = begin; it != end; ++it) {
+            min = *it < *min ? it : min;
+        }
+        return *min;
+    }
+
+    template<typename iterator>
+    inline decltype(iterator::operator*) max(iterator begin, iterator end) {
+        if (end - begin == 0) {
+            throw std::length_error("empty sequence");
+        }
+
+        iterator max = begin;
+        for (auto it = begin; it != end; ++it) {
+            max = *it > *max ? it : max;
+        }
+        return *max;
+    }
+
+    template<typename iterator>
+    inline uint64_t argmin(iterator begin, iterator end) {
+        if (end - begin == 0) {
+            throw std::length_error("empty sequence");
+        }
+
+        iterator min = begin;
+        for (auto it = begin; it != end; ++it) {
+            min = *it < *min ? it : min;
+        }
+        return min - begin;
+    }
+
+    template<typename iterator>
+    inline uint64_t argmax(iterator begin, iterator end) {
+        if (end - begin == 0) {
+            throw std::length_error("empty sequence");
+        }
+
+        iterator max = begin;
+        for (auto it = begin; it != end; ++it) {
+            max = *it > *max ? it : max;
+        }
+        return max - begin;
+    }
+}
+
+namespace io {
+    /***
+     * generalized STL-like print function
+     * @tparam iterator
+     * @param os
+     * @param begin
+     * @param end
+     * @param b
+     * @param e
+     * @param print_func
+     */
+    template <typename iterator>
+    inline void print(std::ostream &os, iterator begin, iterator end, char b, char e,
+    std::function<void (std::ostream &os, iterator it)> &print_func){
+    auto it = begin;
+    os << b;
+    while (it != end) {
+        print_func(os, it);
+        auto _it = it;
+        if (++_it != end) {
+            os << ", ";
+        }
+    }
+    os << e;
+}
+
+template <typename iterator>
+inline void print(std::ostream &os, iterator begin, iterator end, char b='(', char e=')'){
+    auto it = begin;
+    os << b;
+    while (it != end) {
+        os << *it;
+        auto _it = it;
+        if (++_it != end) {
+            os << ", ";
+        }
+        ++it;
+    }
+    os << e;
+}
+}
+
+template <typename T>
+inline std::ostream& operator<<(std::ostream &os, const std::vector<T> &vec) {
+    io::print(os, vec.begin(), vec.end(), '[', ']');
+    return os;
+}
+
+template <typename T, size_t N>
+inline std::ostream& operator<<(std::ostream &os, const std::array<T, N> &array) {
+    io::print(os, array.begin(), array.end(), '[', ']');
+    return os;
+}
+
+template <typename T>
+inline std::ostream& operator<<(std::ostream &os, const std::set<T> &set) {
+    io::print(os, set.begin(), set.end(), '{', '}');
+    return os;
+}
+
+template <typename T>
+inline std::ostream& operator<<(std::ostream &os, const std::unordered_set<T> &set) {
+    io::print(os, set.begin(), set.end(), '{', '}');
+    return os;
+}
+
+template <typename K, typename V>
+inline std::ostream& operator<<(std::ostream &os, const std::unordered_map<K, V> &map) {
+    io::print(os, map.begin(), map.end(), '{', '}', [](std::ostream &os, decltype(map.begin()) it) { os << it->first << ": " << it->second; });
+    return os;
+}
+
+template <typename K, typename V>
+inline std::ostream& operator<<(std::ostream &os, const std::map<K, V> &map) {
+    io::print(os, map.begin(), map.end(), '{', '}', [](std::ostream &os, decltype(map.begin()) it) { os << it->first << ": " << it->second; });
+    return os;
+}
+
+/***
+ * byte swap using gcc built-ins for various interger types
+ * @tparam T integer type
+ * @param x value to byte swap
+ * @return
+ */
+template<typename T>
+inline T bswap(T x) {
+    if (std::is_same<T, int8_t>::value || std::is_same<T, uint8_t>::value) {
+        return (T) x;
+    } else if (std::is_same<T, int16_t>::value || std::is_same<T, uint16_t>::value) {
+        return (T) __bswap_16((uint16_t) x);
+    } else if (std::is_same<T, int32_t>::value || std::is_same<T, uint32_t>::value) {
+        return (T) __bswap_32((uint32_t) x);
+    } else if (std::is_same<T, int64_t>::value || std::is_same<T, uint64_t>::value) {
+        return (T) __bswap_64((uint64_t) x);
+    } else {
+        return (T) x;
+    }
+}
+
+/***
+ * convert data type to network byte order and back
+ * @tparam T
+ * @param x
+ * @return
+ */
+template <typename T>
+inline T inet_bswap(T x) {
+#if BYTE_ORDER == LITTLE_ENDIAN
+    return bswap<T>(x);
+#else
+    return (T) x;
+#endif
+}
+
+inline bool exists(const std::string& fname) {
+    return (access(fname.c_str(), F_OK ) != -1);
+}
+
+#endif // __UTIL_HPP
--- src/main.cu
+++ src/main.cu
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <cuda_helpers.cuh>
 #include <opencv2/opencv.hpp>
+#include <common.hpp>
 
 #ifdef CUDA_SHARED_MEMORY
 #define CUDA_MALLOC(x, len)         x = malloc(len)
@@ -15,60 +16,117 @@
 #define CUDA_D2H(dest, src, len)    cudaMemcpy(dest, src, len, D2H);
 #endif
 
-static cv::Mat equalizeHistogram(cv::InputArray in) {
-    cv::Mat out;
-    cv::equalizeHist(in, out);
+#define DEFAULT_NUM_FRAMES  1
+#define DEFAULT_CLIP_LIMIT  40.0
+#define DEFAULT_GRID_SIZE   8
+
+static std::vector<cv::Mat> equalizeHistogram(const std::vector<cv::Mat> &in) {
+    std::vector<cv::Mat> out(in.size());
+    for (int i = 0; i < in.size(); ++i) {
+        cv::equalizeHist(in[i], out[i]);
+    }
     return out;
 }
 
-static cv::Mat claheNative(cv::InputArray in, double clipLimit=40.0, const cv::Size &gridSize=cv::Size(8, 8)) {
-    cv::Mat out;
+static std::vector<cv::Mat> claheNative(const std::vector<cv::Mat> &in, double clipLimit, const cv::Size &gridSize) {
+    std::vector<cv::Mat> out(in.size());
     auto clahe = cv::createCLAHE(clipLimit, gridSize);
-    clahe->apply(in, out);
+    for (int i = 0; i < in.size(); ++i) {
+        clahe->apply(in[i], out[i]);
+    }
     return out;
 }
 
-static cv::Mat claheCuda(cv::InputArray in, double clipLimit=40.0, const cv::Size &gridSize=cv::Size(8, 8)) {
-    cv::Mat out;
+static std::vector<cv::Mat> claheCuda(const std::vector<cv::Mat> &in, double clipLimit, const cv::Size &gridSize) {
+    std::vector<cv::Mat> out(in.size());
     auto clahe = cv::cuda::createCLAHE(clipLimit, gridSize);
-    clahe->apply(in, out);
+    for (int i = 0; i < in.size(); ++i) {
+        cv::cuda::GpuMat tmp_in(in[i]), tmp_out;
+        clahe->apply(tmp_in, tmp_out);
+        tmp_out.download(out[i]);
+    }
     return out;
+}
+
+static cv::Mat concat(const cv::Mat &topLeft, const cv::Mat &topRight, const cv::Mat &bottomLeft, const cv::Mat &bottomRight) {
+    cv::Mat top, bottom, output;
+    cv::hconcat(topLeft, topRight, top);
+    cv::hconcat(bottomLeft, bottomRight, bottom);
+    cv::vconcat(top, bottom, output);
+    return output;
 }
 
 int main(int argc, const char *argv[]) {
-    std::vector<std::string> args(argv, argv + argc);
+    const std::vector<std::string> args(argv, argv + argc);
     if (args.size() == 1) {
-        std::cout << "Usage: " << args[0] << "[options...] <source>" << std::endl;
+        std::cout << "Usage: " << args[0] << " [options...] <source>" << std::endl;
         return EXIT_SUCCESS;
     }
 
-    const std::string &filename = args[1];
+    unsigned numFrames = DEFAULT_NUM_FRAMES;
+    double clipLimit = DEFAULT_CLIP_LIMIT;
+    cv::Size gridSize = cv::Size(DEFAULT_GRID_SIZE, DEFAULT_GRID_SIZE);
+
+    for (int i = 1; i < args.size() - 1; ++i) {
+        if (string::starts_with(args[i], "--clip-limit=")) {
+            auto tokens = string::split(args[i], "=");
+            if (tokens.size() == 2) {
+                clipLimit = string::to<double>(tokens[1]);
+            }
+        } else if (string::starts_with(args[i], "--grid-size=")) {
+            auto tokens = string::split(args[i], "=");
+            if (tokens.size() == 2) {
+                auto gs = string::to<unsigned>(tokens[1]);
+                gridSize = cv::Size(gs, gs);
+            }
+        } else if (string::starts_with(args[i], "--num-frames=")) {
+            auto tokens = string::split(args[i], "=");
+            if (tokens.size() == 2) {
+                numFrames = string::to<unsigned>(tokens[1]);
+            }
+        } else {
+            std::cout << "unrecognized option '" << args[i] << '\'' << std::endl;
+        }
+    }
+
+    std::cout << "numFrames=" << numFrames << std::endl;
+    std::cout << "clipLimit=" << clipLimit << std::endl;
+    std::cout << "gridSize=" << gridSize << std::endl;
+
+    const std::string &filename = args[args.size() - 1];
     cv::Mat frame = cv::imread(filename, cv::IMREAD_GRAYSCALE);
     if (frame.empty()) {
         std::cout << "failed to read source" << std::endl;
         return EXIT_FAILURE;
     }
+    std::cout << "imageSize=[" << frame.rows << " x " << frame.cols << ']' << std::endl;
 
-    cv::Mat m0, m1, m2;
+    std::vector<cv::Mat> data(numFrames);
+    for (int i = 0; i < data.size(); ++i) {
+        frame.copyTo(data[i]);
+    }
+
+    std::cout << std::endl;
+
+    std::vector<cv::Mat> m(3);
     TIMERSTART(equalizeHistogram);
-    m0 = equalizeHistogram(frame);
+    m[0] = equalizeHistogram(data)[0];
     TIMERSTOP(equalizeHistogram);
 
     TIMERSTART(claheNative);
-    m1 = claheNative(frame);
+    m[1] = claheNative(data, clipLimit, gridSize)[0];
     TIMERSTOP(claheNative);
 
     TIMERSTART(claheCuda);
-    m2 = claheCuda(frame);
+    m[2] = claheCuda(data, clipLimit, gridSize)[0];
     TIMERSTOP(claheCuda);
 
-    cv::namedWindow("equalizeHistogram");
-    cv::namedWindow("claheNative");
-    cv::namedWindow("claheCuda");
+    cv::namedWindow("output", cv::WINDOW_AUTOSIZE);
 
-    cv::imshow("equalizeHistogram", m0);
-    cv::imshow("claheNative", m1);
-    cv::imshow("claheCuda", m2);
+    cv::Mat output = concat(frame, m[0], m[1], m[2]);
+
+    cv::imshow("output", output);
+    cv::waitKey(0);
 
     return EXIT_SUCCESS;
 }
